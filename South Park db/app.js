1988f3ef0782f247e46e005162e49f69a662a4f0
Master Z <124045808+Tunafan@users.noreply.github.com>
2023-03-23 14:37:40 +0100

    Add files via upload

---
+++ South Park db/app.js
@@ -0,0 +1,47 @@
+"use strict";
+window.addEventListener("load", initApp);
+
+async function initApp() {
+  console.log(`.js gang gang`);
+  let harry = await getCharacter(
+    `https://raw.githubusercontent.com/cederdorff/dat-js/main/data/harry.json`
+  );
+  showCharacter(harry);
+
+  let draco = await getCharacter(
+    `https://raw.githubusercontent.com/cederdorff/dat-js/main/data/draco.json`
+  );
+  showCharacter(draco);
+
+  let paris = await getCharacter("data/characters.json");
+  showCharacter(paris);
+}
+
+async function getCharacter(url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
+
+function showCharacter(character) {
+  console.log(character);
+
+  document.querySelector("body").insertAdjacentHTML(
+    "beforebegin",
+    /*html*/ `
+    <article>
+        <h2>${character.name}</h2>
+        <img src="${character.image}" id="lol" alt="" />
+        <header>${character.nickname}</header>
+        <header>${character.occupation}</header>
+        <header>${character.age}</header>
+        <header>${character.voicedBy}</header>
+        <header>${character.gender}</header>
+        <header>${character.catchPhrase}</header>
+        <header>${character.hairColor}</header>
+        <header>${character.episodes}</header>
+        <header>${character.appearances}</header>
+        <header>${character.firstAppearance}</header>
+        </article>`
+  );
+}
